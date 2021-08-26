b3c77ce83617d6b6c1992276b7aae9720869cb13
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2021-08-26 14:59:10 +0200

    Increase tree exploration performance

--- analysis/tree_exploration.py
+++ analysis/tree_exploration.py
@@ -17,23 +17,23 @@ def calculate_statistics(sc_data):
     genes = sc_data.var.index.array
     data = sc_data.X
 
-    means = pd.DataFrame(index=cell_types, columns=genes)
-    factors = pd.DataFrame(index=cell_types, columns=genes)
+    means = {}
+    factors = {}
 
     for cell_type in cell_types:
         filtered = data[sc_data.obs["cell_type"] == cell_type].todense()
         totals = filtered.sum(0)
-        factors.loc[cell_type] = np.ones(shape=len(genes)) - (filtered.std(0) * 2)
+        factors[cell_type] = np.ones(shape=len(genes), dtype="float32") - (filtered.std(0) * 2)
         # factors.loc[cell_type] = np.ones(shape=len(genes)) - maxs - mins
-        means.loc[cell_type] = totals / totals.sum()
+        means[cell_type] = np.array(totals / totals.sum(), dtype="float32")
 
     return means, factors
 
 
 # Assumes b is without error
 def distance(a, b):
-    scaled_diff = (a[0] - b[0]) / a[1]
-    return (scaled_diff ** 2).sum()
+    scaled_diff = np.divide((a[0] - b[0]), a[1])
+    return np.square(scaled_diff).sum()
 
 
 def interpolate(a, b, x):
@@ -48,7 +48,7 @@ def analyze(sc_data, st_data):
     sc.pp.normalize_total(sc_data, target_sum=1)
     means, factors = calculate_statistics(sc_data)
     cells = st_data.obs.index.array
-    cell_types = means.index.array
+    cell_types = means.keys()
     err = 0
     total_miss = 0
     total_dist = 0
@@ -56,24 +56,24 @@ def analyze(sc_data, st_data):
     for cell in tqdm(cells):
         excerpt = st_data[cell]
         target_counts = excerpt.X.todense().A1
-        target = (target_counts, np.zeros(target_counts.size))
-        current = (np.zeros(target_counts.size), np.zeros(target_counts.size))
-        # target = excerpt.X.todense().A1
-        # current = np.zeros(target.size)
+        # target = (target_counts, np.zeros(target_counts.size))
+        # current = (np.zeros(target_counts.size), np.zeros(target_counts.size))
+        target = excerpt.X.todense().A1
+        current = np.zeros(target.size)
         found_cells = []
         for step in range(TREE_DEPTH):
             best = inf
             best_coord = current
             best_type = None
             for cell_type in cell_types:
-                new_coord = interpolate(
-                    current,
-                    (means.loc[cell_type], factors.loc[cell_type]),
-                    1 - (step / (step + 1))
-                )
-                dist = distance(new_coord, target)
-                # new_coord = (step * current + means.loc[cell_type]) / (step + 1)
-                # dist = (((target - new_coord) * (factors.loc[cell_type])) ** 2).sum()
+                #new_coord = interpolate(
+                #    current,
+                #    (means[cell_type], factors[cell_type]),
+                #    1 - (step / (step + 1))
+                #)
+                #dist = distance(new_coord, target)
+                new_coord = (step * current + means[cell_type]) / (step + 1)
+                dist = np.square(np.multiply((target - new_coord), (factors[cell_type]))).sum()
                 if dist < best:
                     best_type = cell_type
                     best = dist
@@ -101,7 +101,7 @@ def analyze(sc_data, st_data):
             #print(compare)
     print(f"Correct: {len(cells) - err}/{len(cells)}")
     print(f"Misses = {total_miss}")
-    print(f"Distance = {total_dist}")
+    print(f"AVG Distance = {total_dist / len(cells)} (total: {total_dist})")
 
 
 test_sc_data = ad.read(SC_FILE)
